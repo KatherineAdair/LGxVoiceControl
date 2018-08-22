9511d2c69475fdb4d9e5da0eb610458ab970080f
Omshi Samal <omshisamal72@gmail.com>
2018-08-22 15:59:30 +0530

    Create write-query.py

---
+++ write-query.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os, json
+
+with open('/path/to/data.json') as json_data: #getting data from firebase
+    data = json.load(json_data)
+
+query = open("/path/to/query.txt", "w") #creating query.txt
+os.chown("/path/to/query.txt", <UserId>, <UserId>) #changing file owner to prevent google earth from re-reading the file
+
+command_type = data["command"]
+
+if command_type=="heading":
+	query.write("flytoview=<LookAt><heading>"+str(data["value"]["0"])+"</heading></LookAt>")
+
+elif command_type=="goto":
+	a= str(data["location"]) #getting location
+	a1="': u'"
+	a2="'}"
+	query.write("search="+a[(a.index(a1)+len(a1)):a.index(a2)])
+
+else:
+	query.write(" ")
+
+query.close()
